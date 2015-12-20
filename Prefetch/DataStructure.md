6e9763c7b9261586a6f69201c5ec31640492b26c
David <davidhowell.tx@gmail.com>
2015-12-20 12:17:58 -0600

    Updated Windows XP section

--- Prefetch/DataStructure.md
+++ Prefetch/DataStructure.md
@@ -2,6 +2,95 @@
 # Windows Prefetch Data Structures
 
 
+# Windows XP
+
+## File Header - 84 Bytes
+| Offset | Length | Value    | Value Format            | Notes |
+| ------ | ------ | -------- | ----------------------- |       |
+| 0      | 4      | 11000000 | Hexadecimal             | Format Version |
+| 4      | 4      | SCCA     | ASCII                   | Signature |
+| 8      | 4      | 0F000000 | Hexadecimal             | Unknown Purpose |
+| 12     | 4      |          | Unsigned 32-bit Integer | Prefetch File Size |
+| 16     | 60     |          | Unicode String          | Name of Executable - 29 characters with U+0000 end-of-string character |
+| 76     | 4      |          | Hexadecimal String      | Prefetch Hash |
+| 80     | 4      |          |                         | Unknown (flags?) |
+
+## File Information - 68 Bytes
+| Offset | Length | Value | Value Format                       | Notes |
+| ------ | ------ | ----- | ---------------------------------- | ----- |
+| 84     | 4      |       | Unsigned 32-bit Integer            | Offset from start of file to Section A |
+| 88     | 4      |       | Unsigned 32-bit Integer            | Number of entries in Section A |
+| 92     | 4      |       | Unsigned 32-bit Integer            | Offset from start of file to Section B |
+| 96     | 4      |       | Unsigned 32-bit Integer            | Number of entries in Section B |
+| 100    | 4      |       | Unsigned 32-bit Integer            | Offset from start of file to Section C |
+| 104    | 4      |       | Unsigned 32-bit Integer            | Length of Section C |
+| 108    | 4      |       | Unsigned 32-bit Integer            | Offset from start of file to Section D |
+| 112    | 4      |       | Unsigned 32-bit Integer            | Number of entries in Section D |
+| 116    | 4      |       | Unsigned 32-bit Integer            | Length of Section D |
+| 120    | 8      |       | Unsigned 64-bit Integer Epoch Time | Last Execution Time |
+| 128    | 16     |       |                                    | Unknown |
+| 144    | 4      |       | Unsigned 32-bit Integer            | Execution Count |
+| 148    | 4      |       |                                    | Unknown |
+
+## Metrics Array
+Metrics Entry Records are 20 bytes each. Offsets are shown from the start of the metrics entry.
+| Offset | Length | Value | Value Format            | Notes  |
+| ------ | ------ | ----- | ----------------------- | ------ |
+| 0      | 4      |       |                         | Unknown |
+| 4      | 4      |       |                         | Unknown |
+| 8      | 4      |       | Unsigned 32-bit Integer | Filename String Offset |
+| 12     | 4      |       | Unsigned 32-bit Integer | Number of Unicode Characters in Filename |
+| 16     | 4      |       |                         | Unknown |
+
+## Trace Chains Array
+Trace Chains Entry Records are 12 bytes each. Offsets are shown from the start of the Trace Chains entry record.
+| Offset | Length | Value Format | Notes |
+| ------ | ------ | ------------ | ----- |
+| 0      | 4      |              | Next array entry index |
+| 4      | 4      |              | Total block load count |
+| 8      | 1      |              | Unknown |
+| 9      | 1      |              | Unknown (Sample duration in milliseconds?) |
+| 10     | 2      |              | Unknown |
+
+## Filename Strings Array
+Filename Strings array contains Unicode Filename Strings terminated by the U+0000 end-of-string character. The offset of each string is recorded in the Metrics Array recorded as the offset from the start of the Filename Strings array. The length of each string in Unicode character is also recorded in the Metrics array.
+
+## Volume Information Array
+Each volume referenced in the Filename Strings array will also contain an entry in the Volume Information array. 
+Each Volume Information array contains information about the volume as well as an NTFS file references array, and a directory strings array.
+
+Volume Information entries are 40 bytes each. Offsets are shown from the start of the Volume Information entry record.
+
+| Offset | Length | Value Format                       | Notes |
+| ------ | ------ | ---------------------------------- | ----- |
+| 0      | 4      | Unsigned 32-bit Integer            | Offset from start of Section D to volume device path |
+| 4      | 4      | Unsigned 32-bit Integer            | Length of volume device path in Unicode characters |
+| 8      | 8      | Unsigned 64-bit Integer Epoch time | Volume Creation Time |
+| 16     | 4      | Hexadecimal String                 | Volume serial number |
+| 20     | 4      | Unsigned 32-bit Integer            | Offset from start of Section D to Subsection E |
+| 24     | 4      | Unsigned 32-bit Integer            | Length of Subsection E in bytes |
+| 28     | 4      | Unsigned 32-bit Integer            | Offset from start of Section D to Subsection F |
+| 32     | 4      | Unsigned 32-bit Integer            | Number of strings in Subsection F |
+| 36     | 4      |                                    | Unknown |
+
+### NTFS File Reference Array
+NTFS File reference entries are 8 bytes in size.
+
+| Offset | Length | Value Format | Notes |
+| ------ | ------ | ------------ | ----- |
+| 0      | 6      |              | MFT Entry Index |
+| 6      | 2      |              | Sequence Number |
+
+### Directory Strings Array
+An array of Unicode directory strings associated with files accessed by the executable. The number of string entries is stored in the Volume Information array.
+
+Each directory string entry has the following structure:
+| Offset | Length | Value Format            | Notes |
+| ------ | ------ | ----------------------- | ----- |
+| 0      | 2      | Unsigned 16-bit Integer | Length of directory string in Unicode characters |
+| 2      |        | Unicode string          | Directory Name |
+|        | 2      | Unicode                 | U+0000 end-of-string character |
+
 # Windows 7
 
 ## Overview of Sections
--- Prefetch/Invoke-PrefetchParser.ps1
+++ Prefetch/Invoke-PrefetchParser.ps1
@@ -30,8 +30,12 @@ if (Test-Path -Path $FilePath) {
 	# First 4 Bytes - Version Indicator
 	$Version = [System.BitConverter]::ToString($BinReader.ReadBytes(4)) -replace "-",""
 	
-	# Next 8 Bytes are "SCCA" Signature, and 4 Bytes for unknown purpose either 0x0F000000 for WinXP or 0x11000000 for Win7/8
-	[System.BitConverter]::ToString($BinReader.ReadBytes(8)) -replace "-","" | Out-Null
+	# Next 4 Bytes are "SCCA" Signature
+	$ASCIIEncoding.GetString($BinReader.ReadBytes(4)) | Out-Null
+	
+	# Next 4 Bytes are of unknown purpose
+	# Value is 0x0F000000 for WinXP or 0x11000000 for Win7/8
+	[System.BitConverter]::ToString($BinReader.ReadBytes(4)) -replace "-","" | Out-Null
 	
 	switch ($Version) {
 		# Windows XP Structure
@@ -42,42 +46,112 @@ if (Test-Path -Path $FilePath) {
 			$TempObject | Add-Member -MemberType NoteProperty -Name "PrefetchSize" -Value ([System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0))
 			$TempObject.Name = $UnicodeEncoding.GetString($BinReader.ReadBytes(60))
 			$TempObject.Hash = [System.BitConverter]::ToString($BinReader.ReadBytes(4)) -replace "-",""
-			$FileStream.Position = 100
-			$TempObject | Add-Member -MemberType NoteProperty -Name "FilePathsOffset" -Value ([System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0))
-			$TempObject | Add-Member -MemberType NoteProperty -Name "FilePathsLength" -Value ([System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0))
-			$TempObject | Add-Member -MemberType NoteProperty -Name "VolumeInfoOffset" -Value ([System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0))
-			$TempObject | Add-Member -MemberType NoteProperty -Name "VolumeInfoCount" -Value ([System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0))
-			$TempObject | Add-Member -MemberType NoteProperty -Name "VolumeInfoSize" -Value ([System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0))
-			$BinReader.ReadBytes(8) | Out-Null
+			
+			# Unknown Flags
+			$BinReader.ReadBytes(4) | Out-Null
+			# Offset to Section A (Metrics Array)
+			$MetricsArrayOffset = [System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0)
+			# Number of Entries in Section A (Metrics Array)
+			$MetricsArrayEntries  = [System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0)
+			# Offset to Section B (Trace Chains Array)
+			$BinReader.ReadBytes(4) | Out-Null
+			# Number of Entries in Section B (Trace Chains Array)
+			$BinReader.ReadBytes(4) | Out-Null
+			# Offset to Section C (File Names Array)
+			$FilenamesArrayOffset = [System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0)
+			# Length of Section C (File Names Array)
+			$FilenamesArrayLength = [System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0)
+			# Offset to Section D (Volume Information Array)
+			$VolumeInfoArrayOffset = [System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0)
+			# Number of Entries in Section D (Volume Information Array)
+			$VolumeInfoArrayEntries = [System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0)
+			# Length of Section D (Volume Information Array)
+			$VolumeInfoArrayLength = [System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0)
+			# Last Execution Time
 			$TempObject.LastExecutionTime = [DateTime]::FromFileTime([System.BitConverter]::ToUInt64($BinReader.ReadBytes(8),0)).ToString("G")
+			# Unknown
+			$BinReader.ReadBytes(16) | Out-Null
+			# Execution Count
+			$TempObject.NumberOfExecutions = [System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0)
+			# Unknown
+			$BinReader.ReadBytes(4) | Out-Null			
 			
-			# Parse File Names
-			$FileStream.Position = $TempObject.FilePathsOffset
-			$FilePathsBlob = $UnicodeEncoding.GetString($BinReader.ReadBytes($TempObject.FilePathsLength)) -split "\\DEVICE\\" | Where-Object { $_ }
-			for ($k=0; $k -lt $FilePathsBlob.Count; $k++) {
-				$TempObject | Add-Member -MemberType NoteProperty -Name "FilePathsBlob_File$($k)" -Value $FilePathsBlob[$k]
+			# Parse Metrics (Section A) and the associated File Names
+			for ($i=1; $i -le $MetricsArrayEntries; $i++) {
+				# Start Time in Milliseconds
+				$BinReader.ReadBytes(4) | Out-Null
+				# Duration in Milliseconds
+				$BinReader.ReadBytes(4) | Out-Null
+				
+				$FileNameOffset = [System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0)
+				$FileNameLength = [System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0)
+				
+				# Unknown Flags
+				$BinReader.ReadBytes(4) | Out-Null
+				
+				# Read the File Name out of Section C using the Offset and Length we just parsed
+				
+				# Store Current Location in Section A so we can come back
+				$CurrentLocation = $FileStream.Position
+				# Change File Stream Position to File Name Offset
+				$FileStream.Position = $FilenamesArrayOffset + $FileNameOffset
+				# Read the File Name
+				$TempObject | Add-Member -MemberType NoteProperty -Name "Filename$($i)" -Value ($UnicodeEncoding.GetString($BinReader.ReadBytes($FileNameLength)))
+				# Change back to location in Section A
+				$FileStream.Position = $CurrentLocation
+				
+				Remove-Variable -Name FileNameOffset
+				Remove-Variable -Name FileNameLength
+				Remove-Variable -Name CurrentLocation
 			}
+			Remove-Variable -Name i
 			
-			$FileStream.Position = $TempObject.VolumeInfoOffset
-			for ($i=1; $i -le $TempObject.VolumeInfoCount; $i++) {
-				# Parse Volume Info
-				$TempObject | Add-Member -MemberType NoteProperty -Name "Volume$($i)_PathOffset" -Value ([System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0))
-				$TempObject | Add-Member -MemberType NoteProperty -Name "Volume$($i)_PathLength" -Value ([System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0))
+			# Parse Section D (Volume Information)
+			$FileStream.Position = $VolumeInfoArrayOffset
+			for ($i=1; $i -le $VolumeInfoArrayEntries; $i++) {
+				$VolumePathOffset = [System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0)
+				$VolumePathLength = [System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0)
+				
 				$TempObject | Add-Member -MemberType NoteProperty -Name "Volume$($i)_CreationTime" -Value ([DateTime]::FromFileTime([System.BitConverter]::ToUInt64($BinReader.ReadBytes(8),0)).ToString("G"))
 				$TempObject | Add-Member -MemberType NoteProperty -Name "Volume$($i)_SerialNumber" -Value ([System.BitConverter]::ToString($BinReader.ReadBytes(4),0) -replace "-","")
-				$TempObject | Add-Member -MemberType NoteProperty -Name "Volume$($i)_OffsetToBlob1" -Value ([System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0))
-				$TempObject | Add-Member -MemberType NoteProperty -Name "Volume$($i)_LengthOfBlob1" -Value ([System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0))
-				$TempObject | Add-Member -MemberType NoteProperty -Name "Volume$($i)_OffsetToFolderPaths" -Value ([System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0))
-				$TempObject | Add-Member -MemberType NoteProperty -Name "Volume$($i)_NumberOfFolderPaths" -Value ([System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0))
-				$FileStream.Position = ($FileStream.Position + $($TempObject."Volume$($i)_PathOffset") - 36)
-				$TempObject | Add-Member -MemberType NoteProperty -Name "Volume$($i)_Path" -Value ($UnicodeEncoding.GetString($BinReader.ReadBytes($($TempObject."Volume$($i)_PathLength") * 2)))
-				$BinReader.ReadBytes(($($TempObject."Volume$($i)_OffsetToBlob1") - $($TempObject."Volume$($i)_PathOffset") - ($($TempObject."Volume$($i)_PathLength") * 2))) | Out-Null
-				$TempObject | Add-Member -MemberType NoteProperty -Name "Volume$($i)_Blob1" -Value ($ASCIIEncoding.GetString($BinReader.ReadBytes($($TempObject."Volume$($i)_LengthOfBlob1"))))
 				
-				for ($j=1; $j -le $($TempObject."Volume$($i)_NumberOfFolderPaths"); $j++) {
-					$TempObject | Add-Member -MemberType NoteProperty -Name "Volume$($i)_FolderPath$($j)Length" -Value ([System.BitConverter]::ToUInt16($BinReader.ReadBytes(2),0))
-					$TempObject | Add-Member -MemberType NoteProperty -Name "Volume$($i)_FolderPath$($j)" -Value ($UnicodeEncoding.GetString($BinReader.ReadBytes($($TempObject."Volume$($i)_FolderPath$($j)Length") * 2)))
-					$BinReader.ReadBytes(2) | Out-Null
+				# Offset to Subsection E (NTFS File References)
+				[System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0) | Out-Null
+				# Length of Subsection E (NTFS File References)
+				[System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0) | Out-Null
+				
+				$DirectoryStringsArrayOffset = [System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0)
+				$DirectoryStringsArrayEntries = [System.BitConverter]::ToUInt32($BinReader.ReadBytes(4),0)
+				
+				#Unknown
+				$BinReader.ReadBytes(4) | Out-Null
+				
+				# Read the Volume Path String
+				$FileStream.Position = $VolumeInfoArrayOffset + $VolumePathOffset
+				$TempObject | Add-Member -MemberType NoteProperty -Name "Voluem$($i)_Path" -Value ($UnicodeEncoding.GetString($BinReader.ReadBytes($VolumePathLength * 2)))
+				
+				# Move to the Directory Strings Array and read the strings
+				$FileStream.Position = $VolumeInfoArrayOffset + $DirectoryStringsArrayOffset
+				for ($j=1; $j -le $DirectoryStringsArrayEntries; $j++) {
+					$DirectoryStringLength = [System.BitConverter]::ToUInt16($BinReader.ReadBytes(2),0)
+					$TempObject | Add-Member -MemberType NoteProperty -Name "Volume$($i)_Directory$($j)" -Value $UnicodeEncoding.GetString($BinReader.ReadBytes($DirectoryStringLength * 2 + 2))
+					Remove-Variable -Name DirectoryStringLength
+				}
+				Remove-Variable -Name VolumePathOffset
+				Remove-Variable -Name VolumePathLength
+				Remove-Variable -Name DirectoryStringsArrayOffset
+				Remove-Variable -Name DirectoryStringsArrayEntries
+				Remove-Variable -Name j
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
 				}
 			}
 		}
